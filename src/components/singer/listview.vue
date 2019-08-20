68c13e3d61612982e2006e7bb9ce09632f5fe7fe
aqibba <aqibba@126.com>
2019-08-20 16:25:02 +0800

    歌手列表

---
+++ src/components/singer/listview.vue
@@ -0,0 +1,101 @@
+<template>
+  <scroll :data="data" class="listview" ref="listview">
+    <ul>
+      <li v-for="(group, index) in data" class="list-group" ref="listGroup" :key="index">
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li v-for="(item, index) in group.items" class="list-group-item" :key="index">
+            <img class="avatar" v-lazy="item.avatar">
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </scroll>
+</template>
+
+<script>
+import Scroll from '../../base/scroll/scroll'
+import Loading from '../../base/loading/loading'
+
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: null
+    }
+  },
+  components: {
+    Scroll,
+    Loading
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+@import "../../common/styles/variable"
+
+.listview
+  position: relative
+  width: 100%
+  height: 100%
+  overflow: hidden
+  background: $color-background
+  .list-group
+    padding-bottom: 1.875rem
+    .list-group-title
+      height: 1.875rem
+      line-height: 1.875rem
+      padding-left: 1.25rem
+      font-size: $font-size-small
+      color: $color-text-l
+      background: $color-highlight-background
+    .list-group-item
+      display: flex
+      align-items: center
+      padding: 1.25rem 0 0 1.875rem
+      .avatar
+        width: 3.125rem
+        height: 3.125rem
+        border-radius: 50%
+      .name
+        margin-left: 1.25rem
+        color: $color-text-l
+        font-size: $font-size-medium
+  .list-shortcut
+    position: absolute
+    z-index: 30
+    right: 0
+    top: 50%
+    transform: translateY(-50%)
+    width: 1.25rem
+    padding: 1.25rem 0
+    border-radius: 0.625rem
+    text-align: center
+    background: $color-background-d
+    font-family: Helvetica
+    .item
+      padding: 0.1875rem
+      line-height: 1
+      color: $color-text-l
+      font-size: $font-size-small
+      &.current
+        color: $color-theme
+  .list-fixed
+    position: absolute
+    top: 0
+    left: 0
+    width: 100%
+    .fixed-title
+      height: 1.875rem
+      line-height: 1.875rem
+      padding-left: 1.25rem
+      font-size: $font-size-small
+      color: $color-text-l
+      background: $color-highlight-background
+  .loading-container
+    position: absolute
+    width: 100%
+    top: 50%
+    transform: translateY(-50%)
+</style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,103 @@
 <template>
-  <div>
-    singer
+  <div class="singer">
+    <list-view :data="singerList"></list-view>
   </div>
 </template>
 
 <script>
+import { getSingerList } from '../../api/singer.js'
+import { ERR_OK } from '../../api/config.js'
+import Loading from '../../base/loading/loading.vue'
+import Singer from '../../common/js/singer.js'
+import ListView from './listview.vue'
+
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+
+export default {
+  data () {
+    return {
+      singerList: []
+    }
+  },
+  created () {
+    this._getSinger()
+  },
+  methods: {
+    _getSinger () {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          this.singerList = this.setSinger(res.data.list)
+          // console.log('================')
+          // console.log(this.setSinger(this.singerList))
+          // console.log('================')
+        }
+      })
+    },
+    // 将得到的singer数据重新规范,
+    setSinger (singerList) {
+      // 热门数据
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      // 遍历得到的singer数据
+      singerList.forEach((item, index) => {
+        // 将得到的10条之内的数据添加进map的item中作为热门的歌手数据
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        }
+        // 根据得到的数据中的Findex做 A-Z 的排序
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          name: item.Fsinger_name,
+          id: item.Fsinger_mid
+        }))
+      })
+      // console.log(map)
+      // 按照 热门 A-Z 的方式排序
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        // console.log(val)
+        // console.log(val.title)
+        // console.log('val.title     ' + typeof (val.title))
+        if (val.title === HOT_NAME) {
+          // debugger
+          hot.push(val)
+        } else {
+          ret.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  },
+  components: {
+    Loading,
+    ListView
+  }
+}
 </script>
 
-<style>
+<style lang="stylus" scoped="scoped">
+.singer
+  position fixed
+  top 5.5rem
+  bottom 0
+  width 100%
 </style>
